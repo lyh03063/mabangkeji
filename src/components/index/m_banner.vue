87de978f8b674dd50eb342178880e2f9e00cebf0
lfh <654029036@qq.com>
2019-08-27 10:03:35 +0800

    修改轮播图样式

--- src/components/index/m_banner.vue
+++ src/components/index/m_banner.vue
@@ -1,6 +1,12 @@
 <template>
   <div class>
-    <el-carousel indicator-position="outside" :height="bannerHeight">
+    <el-carousel indicator-position="outside" height="440px" v-if="!showMovePage">
+      <el-carousel-item v-for="item in arrImg" :key="item.url">
+        <div class="img-box" :style="{'color':`#f00`,'background-image':`url(${item.url})`}"></div>
+        <!-- <el-image  :src="item.url" ></el-image> -->
+      </el-carousel-item>
+    </el-carousel>
+    <el-carousel indicator-position="outside" :height="bannerHeight" v-if="showMovePage">
       <el-carousel-item v-for="item in arrImg" :key="item.url">
         <div class="img-box" :style="{'color':`#f00`,'background-image':`url(${item.url})`}"></div>
         <!-- <el-image  :src="item.url" ></el-image> -->
@@ -17,28 +23,36 @@
 export default {
   data() {
     return {
-      bannerHeight:'440px',
+      showMovePage:false,
+      bannerHeight:'170px',
       arrImg: [
-        { url: "http://pwff5uqxs.bkt.clouddn.com/mabang_baner2.jpg" },
-        { url: "http://www.mobiletrain.org/page/img/html5/banner_pic1.jpg" }
-      ]
+      { url: "http://pwff5uqxs.bkt.clouddn.com/mabang_baner2.jpg" },
+        { url: "http://www.mobiletrain.org/page/img/html5/banner_pic1.jpg" }]
+      
     };
   },
   methods: {
     aaa() {}
   },
   created() {
+    if (window.screen.width<769) {
+      this.showMovePage  = true
+      this.arrImg=[
+        { url: "http://www.mobiletrain.org/d/file/about/money/7a83c36fbc27feba3795223e4a162b6f.jpg" },
+        { url: "http://img.mp.itc.cn/upload/20161107/6f6c5e40f2a94115ba1b3d87a23b73d6_th.jpg" }
+      ]
+    }
     if (window.screen.width<769&&window.screen.width>600) {
-      this.bannerHeight = '230px'
+      this.bannerHeight = '260px'
     }else if (window.screen.width<=600&&window.screen.width>530) {
-      this.bannerHeight = '190px'
+      this.bannerHeight = '240px'
     }else if (window.screen.width<=530&&window.screen.width>460) {
-      this.bannerHeight = '160px'
+      this.bannerHeight = '220px'
     }
     else if (window.screen.width<=460&&window.screen.width>400) {
-      this.bannerHeight = '130px'
+      this.bannerHeight = '200px'
     }else if (window.screen.width<400) {
-      this.bannerHeight = '100px'
+      this.bannerHeight = '180px'
     }
   }
 };
